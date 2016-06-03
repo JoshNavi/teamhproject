a433127ea40bd31ebb130c1046bf08ab8848e776
Jeremy Leu <j3leu@ucsd.edu>
2016-06-02 20:16:38 -0700

    fixed

--- public/js/wip.js
+++ public/js/wip.js
@@ -1,15 +1,22 @@
-
-
 (function(d3) {
   "use strict";
 
-  d3.json("/mood/race", function(err, data) {
+  d3.json("/moodbyrace/race", function(err, data) {
     if (err) {
       console.log(err);
       return;
     }
     makeRaceChart(data);
   });
+
+/*
+  d3.json("/race", function(err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    makeRaceGeographyChart(data);
+  });*/
 
   d3.json("/race", function(err, data) {
     if (err) {
@@ -50,11 +57,16 @@ getColor = function(d) {
   return c20(8);
 }
 
+/*********** STACKED BAR CHART ***********/
 
-makeRaceChart = function(data) {
-  var margin = {top: 20, right: 20, bottom: 30, left: 80},
-    width = window.innerWidth - margin.left - margin.right - 50,
-    height = 700 - margin.top - margin.bottom;
+makeRaceGeographyChart = function(data) {
+
+  //console.log("fuck you");
+  var margin = {top: 20, right: 30, bottom: 150, left: 40},
+    width = window.innerWidth - margin.left - margin.right - 90,
+    height = 600 - margin.top - margin.bottom;
+
+    //console.log(data);
 
   var x = d3.scale.ordinal()
       .rangeRoundBands([0, width], .1);
@@ -62,59 +74,293 @@ makeRaceChart = function(data) {
   var y = d3.scale.linear()
       .range([height, 0]);
 
+  var color = d3.scale.ordinal()
+      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"]);
+
   var xAxis = d3.svg.axis()
       .scale(x)
       .orient("bottom");
 
   var yAxis = d3.svg.axis()
       .scale(y)
-      .orient("left");
-      // .ticks(10, "%");
+      .orient("left")
+      .tickFormat(d3.format(".2s"));
 
-  var svg = d3.select("#link3info").append("svg")
+  var svg = d3.select(".chart1")
+      .append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  x.domain(data.map(function(d) { return d.year + ", " + d.race; }));
-  y.domain([0, d3.max(data, function(d) { return d.rate; })]);
+/* messing around with data */
+  var csvData = [];
+  var areaDict = {};
+  data.map(function (elem) {
+    if (elem.race.indexOf('Any') == 0) return;
 
+    if (!areaDict[elem.area]) {
+      areaDict[elem.area] = {};
+      // areaDict = {
+      //   'alpine': {}
+      // }
+    }
+    areaDict[elem.area][elem.race] = elem.population;
+    // areaDict = {
+    //   'alpine': {
+    //     'black': 999
+    //   }
+    // }
+
+    return;
+  });
+  for (key in areaDict) {
+    var row = {}
+    row.Area = key; // name whatever that column
+    for (race in areaDict[key]) {
+      row[race] = areaDict[key][race];
+    }
+    csvData.push(row);
+  }
+
+  //console.log(csvData);
+
+
+  color.domain(d3.keys(csvData[0]).filter(function(key) { return key !== "Area"; }));
+  csvData.forEach(function(d) {
+    var y0 = 0;
+    d.races = color.domain().map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
+    d.total = d.races[d.races.length - 1].y1;
+  });
+  csvData.sort(function(a, b) { return b.total - a.total; });
+  x.domain(csvData.map(function(d) { return d.Area; }));
+  y.domain([0, d3.max(csvData, function(d) { return d.total; })]);
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
+      .call(xAxis)
+      .selectAll("text")
+              .style("text-anchor", "end")
+              .attr("dx", "-.8em")
+              .attr("dy", ".15em")
+              .attr("transform", function(d) {
+                  return "rotate(-65)"
+                  });
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
-      .append("text")
+    .append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Hospitalization Rate");
-
-  svg.selectAll(".bar")
-      .data(data)
-      .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) {return x(d.year + ", " + d.race); })
+      .text("Population");
+  var state = svg.selectAll(".state")
+      .data(csvData)
+    .enter().append("g")
+      .attr("class", "g")
+      .attr("transform", function(d) { return "translate(" + x(d.Area) + ",0)"; });
+  state.selectAll("rect")
+      .data(function(d) { return d.races; })
+    .enter().append("rect")
       .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.rate); })
-      .attr("height", function(d) { return height - y(d.rate); })
-      .style("fill", function(d) { return getColor(d); });
+      .attr("y", function(d) { return y(d.y1); })
+      .attr("height", function(d) { return y(d.y0) - y(d.y1); })
+      .style("fill", function(d) { return color(d.name); });
+  var legend = svg.selectAll(".legend")
+      .data(color.domain().slice().reverse())
+    .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+  legend.append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d; });
+
+
+
+
+/* correct */
+  //
+  // x.domain(data.map(function(d) { return d.area; }));
+  // y.domain([0, d3.max(data, function(d) { return d.population/1; })]);
+  //
+  // svg.append("g")
+  //     .attr("class", "x axis")
+  //     .attr("transform", "translate(0," + height + ")")
+  //     .call(xAxis)
+  //     .selectAll("text")
+  //         .style("text-anchor", "end")
+  //         .attr("dx", "-.8em")
+  //         .attr("dy", ".15em")
+  //         .attr("transform", function(d) {
+  //             return "rotate(-65)"
+  //             });
+  //
+  // svg.append("g")
+  //     .attr("class", "y axis")
+  //     .call(yAxis)
+  //     .append("text")
+  //     .attr("transform", "rotate(-90)")
+  //     .attr("y", 6)
+  //     .attr("dy", ".71em")
+  //     .style("text-anchor", "end")
+  //     .text("Population");
+  //
+  // svg.selectAll(".bar")
+  //     .data(data)
+  //     .enter().append("rect")
+  //     .attr("class", "bar")
+  //     .attr("x", function(d) {return x(d.area); })
+  //     .attr("width", x.rangeBand())
+  //     .attr("y", function(d) { return y(d.population/1); })
+  //     .attr("height", function(d) {return height - y(d.population/1); })
+  //     .style("fill", function(d) { return getColor(d); });
+}
+
+/*********************************************/
+
+
+
+
+/*********** MOOD BAR CHART ***********/
+
+
+makeRaceChart = function(data) {
+
+  var margin = {top: 20, right: 20, bottom: 180, left: 80},
+      width = 960 - margin.left - margin.right,
+      height = 600 - margin.top - margin.bottom;
+
+  var x0 = d3.scale.ordinal()
+      .rangeRoundBands([0, width], .1);
+
+  var x1 = d3.scale.ordinal();
+
+  var y = d3.scale.linear()
+      .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+      .scale(x0)
+      .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .ticks(10, "%");
+
+  var svg = d3.select(".chart2").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+      var csvData = [];
+      var areaDict = {};
+      data.map(function (elem) {
+        if (!areaDict[elem.geography]) {
+          areaDict[elem.geography] = {};
+        }
+        areaDict[elem.geography][elem.year] = elem.rate;
+        return;
+      });
+      for (key in areaDict) {
+        var row = {}
+        row.Geography = key;
+        for (year in areaDict[key]) {
+          row[year] = areaDict[key][year];
+        }
+        csvData.push(row);
+      }
+
+  console.log("hello world");
+  console.log(csvData);
+
+  var yearNames = d3.keys(csvData
+    [0]).filter(function(key) { return key !== "Geography"; });
+  console.log(yearNames);
+
+
+
+  csvData.forEach(function(d) {
+    d.rates = yearNames.map(function(name) { return {name: name, value: +d[name]}; });
+    console.log(d.rates);
+  });
+
+  x0.domain(csvData.map(function(d) { return d.Geography; }));
+  x1.domain(yearNames).rangeRoundBands([0, x0.rangeBand()]);
+  y.domain([0, d3.max(csvData, function(d) { return d3.max(d.rates, function(d) { return d.value; }); })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .selectAll("text")
+              .style("text-anchor", "end")
+              .attr("dx", "-.8em")
+              .attr("dy", ".15em")
+              .attr("transform", function(d) {
+                  return "rotate(-65)"
+                  });
+
+  svg.append("g")
+  .attr("class", "y axis")
+  .call(yAxis)
+.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", 6)
+  .attr("dy", ".71em")
+  .style("text-anchor", "end")
+  .text("Population");
+
+  var state = svg.selectAll(".state")
+      .data(csvData)
+    .enter().append("g")
+      .attr("class", "state")
+      .attr("transform", function(d) { return "translate(" + x0(d.Geography) + ",0)"; });
+
+  state.selectAll("rect")
+      .data(function(d) { return d.rates; })
+    .enter().append("rect")
+      .attr("width", x1.rangeBand())
+      .attr("x", function(d) { return x1(d.name); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); })
+      .style("fill", function(d) { return color(d.name); });
+
+  var legend = svg.selectAll(".legend")
+      .data(yearNames.slice().reverse())
+    .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  legend.append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d; });
 
 }
 
-
-
+/**********************************/
 
 getGeographyColor = function(d) {
 
-  var margin = {top: 20, right: 30, bottom: 150, left: 40},
-    width = window.innerWidth - margin.left - margin.right - 90,
-    height = 600 - margin.top - margin.bottom;
   if(d.race == "API"){
     return "Yellow";
   }
@@ -148,71 +394,6 @@ makeHospitalizationArc = function(data) {
 
 }
 
-
-makeRaceGeographyChart = function(data) {
-  var margin = {top: 20, right: 30, bottom: 150, left: 70},
-    width = window.innerWidth - margin.left - margin.right - 30,
-    height = 500 - margin.top - margin.bottom;
-
-  var x = d3.scale.ordinal()
-      .rangeRoundBands([0, width], .1);
-
-  var y = d3.scale.linear()
-      .range([height, 0]);
-
-  var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
-
-  var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left")
-      .ticks(5);
-
-  var svg = d3.select("#link3info").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  x.domain(data.map(function(d) { return d.area; }));
-  y.domain([0, d3.max(data, function(d) { return d.population/1; })]);
-
-  console.log(y(10000));
-
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-      .selectAll("text")
-          .style("text-anchor", "end")
-          .attr("dx", "-.8em")
-          .attr("dy", ".15em")
-          .attr("transform", function(d) {
-              return "rotate(-65)"
-              });
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Population");
-
-  svg.selectAll(".bar")
-      .data(data)
-      .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) {return x(d.area); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.population/1); })
-      .attr("height", function(d) {return height - y(d.population/1); })
-      .style("fill", function(d) { return getColor(d); });
-
-}
 
 
 
@@ -285,7 +466,7 @@ makeHospitalizationArc = function(data) {
       })
       .attr("text-anchor", "middle") //center the text on it's origin
       .style("fill", "White")
-      .style("font", "30px Dosis")
+      .style("font", "bold 30px Dosis")
       .text(function(d,i){ return dataset[i].label; });
 
 
@@ -311,10 +492,11 @@ makeHospitalizationArc = function(data) {
   }
 
 
+
+
 }
 
 expandGraph = function(d, i) {
-  console.log(d);
 
   if(d.data.label == "Anxiety Disorders")
   {
@@ -351,7 +533,11 @@ expandGraph = function(d, i) {
 }
 
 makeMoodPie = function(data) {
-    console.log("ArchExpand blah blah blah");
+    // console.log("ArchExpand blah blah blah");
+    // console.log(data);
+
+    // console.log("Mood Pie Working");
+
     console.log(data);
 
     var totalRace = {
@@ -364,8 +550,8 @@ makeMoodPie = function(data) {
 
     for(var i = 0; i < data.length; i++)
     {
-      console.log("test");
-      console.log(data[i].race);
+      // console.log("test");
+      // console.log(data[i].race);
       if(data[i].race == "API")
       {
         totalRace.API += data[i].rate;
@@ -393,7 +579,8 @@ makeMoodPie = function(data) {
 
     }
 
-    console.log(totalRace.API);
+    // console.log(totalRace.API);
+
 
     var pieData = [
       {
@@ -423,7 +610,33 @@ makeMoodPie = function(data) {
       }
     ];
 
-    console.log(pieData);
+    var line1 = d3.select("#line1")
+      .select("svg")
+      .remove("svg");
+
+    var w = 1000;
+    var h = 300;
+
+    var lsvg = d3.select("#line1")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h)
+      .attr("id", "theLine");
+
+    var line1 = lsvg.append("line")
+      .style("stroke", "steelblue")
+      .attr("stroke-width", "5");
+
+    line1
+      .attr("x1", 500)
+      .attr("y1", 0)
+      .attr("x2", 500)
+      .attr("y2", 300)
+      .transition()
+        .duration(3000)
+        .ease("linear")
+        .attr("stroke-dashoffset", 0);
+
     var chart = d3.select("#expandedChart")
       .select("svg")
       .remove("svg");
@@ -431,6 +644,9 @@ makeMoodPie = function(data) {
     var margin = {top: 0, right: 20, bottom: 0, left: 20},
     width = window.innerWidth - margin.left - margin.right,
     height = 1300 - margin.top - margin.bottom;
+
+    var max = d3.max( data.map(function(d){ return parseInt(d.total); }) );
+    var sum = d3.sum( data.map(function(d){ return parseInt(d.total); }) );
 
     var pie = d3.layout.pie()
       .value(function(d) {
@@ -441,6 +657,7 @@ makeMoodPie = function(data) {
       .outerRadius(250);
 
 
+
     var colors = d3.scale.category20c();
 
     var chart = d3.select("#expandedChart")
@@ -448,33 +665,101 @@ makeMoodPie = function(data) {
       .attr("width", width)
       .attr("height", height)
       .append("g")
-      // .attr("transform", "translate(" + width / 2 + ", " + height / 4 + ")");
-      .attr("transform", "translate(" + width / 2 + ", " + height / 5 + ")")
-      .selectAll('path').data(pie(pieData))
-      .enter().append('path')
-        .attr('fill', function(d, i){ return colors(i); })
-        .attr('d', arc);
+      .attr("transform", "translate(" + width / 2 + ", " + height / 5 + ")");
 
-    var g = svg.selectAll(".arc")
-                .data(pie(data))
-                .enter().append("g")
-                .attr("class", "arc");
+    var arc = d3.svg.arc()
+      .outerRadius(250);
 
-            g.append("path")
-                .attr("d", arc)
-                .style("fill", function(d,i) {
-                    return color(i);
-                });
+    var pie = d3.layout.pie()
+      .value(function(d) {
+        return d.value;
+      });
 
-            g.append("text")
-                .attr("transform", function(d) {
-                    return "translate(" + arc.centroid(d) + ")";
-                })
-                .attr("dy", ".35em")
-                .style("text-anchor", "middle")
-                .text(function(d) {
-                  console.log("d is", d);
-                    return percentageFormat(d.data.percentage);
-                });
+    var arcs = chart.selectAll('g.slice').data(pie(pieData))
+        .enter().append('g')
+        .attr("class", "booty");
+
+    arcs.append("path")
+      .attr('d', arc)
+      .style('fill', function(d, i){ return colors(i); })
+        .on("click", function(d, i) { return expandRaceA(d) });
+
+
+
+   //console.log(pieData[1].value);
+   arcs.append("svg:text")
+       .attr("transform", function(d) { //set the label's origin to the center of the arc
+         //we have to make sure to set these before calling arc.centroid
+         d.outerRadius = radius + 50; // Set Outer Coordinate
+         d.innerRadius = radius + 30; // Set Inner Coordinate
+         return "translate(" + arc.centroid(d) + ")";
+       })
+       .attr("text-anchor", "middle") //center the text on it's origin
+       .style("fill", "Black")
+       .style("font", "bold 30px Arial")
+       .text(function(d, i){
+        //console.log(pieData[i].value);
+        return pieData[i].value; });
+
+
+
+    // Computes the angle of an arc, converting from radians to degrees.
+    function angle(d) {
+      var a = (d.startAngle + d.endAngle) * 90 / Math.PI - 90;
+      return a > 90 ? a - 180 : a;
+    }
+
+  var xCoor = -60;
+  var yCoor = 20;
+
+  var legendRectSize = 50;
+  var legendSpacing = 4;
+
+  var legend = chart.selectAll('.legend')
+    .data( pieData )
+    .enter()
+    .append('g')
+    .attr('class', 'legend')
+    .attr('transform', function(d, i) {
+      var height = legendRectSize + legendSpacing;
+      var offset =  height * color.domain().length / 2;
+      var horz = 6 * legendRectSize;
+      var vert = i * height - offset - 150;
+      return 'translate(' + horz + ',' + vert + ')';
+    })
+    .style('float', 'right');
+
+
+    legend.append('rect')                                     // NEW
+      .attr('width', legendRectSize)                          // NEW
+      .attr('height', legendRectSize)                         // NEW
+      .style('fill', function(d, i) { return colors(i); })                                   // NEW
+      .style('stroke', color);                               // NEW
+
+    legend.append('text')                                     // NEW
+      .attr('x', legendRectSize + legendSpacing)              // NEW
+      .attr('y', legendRectSize - legendSpacing)              // NEW
+      .text(function(d,i) { return d.race; })
+      .attr("transform", "translate(" + 10 + "," + -15  + ")")
+      .style("font", "18px Arial");
+
+  }
+
+
+  expandRaceA = function(d, i) {
+
+    console.log(d.data.race);
+    if(d.data.label == "Anxiety Disorders")
+    {
+      d3.json("/anxiety/race", function(err, data) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        makeAnxietyPie(data);
+      });
+    }
+
+
 
   }
